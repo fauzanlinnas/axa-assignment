3b1ec8aad3a6de4e5e7c234c8a4f51a082fed319
Fauzan <fauzan.linnas@ifg-life.id>
2023-04-05 13:48:17 +0700

    integrate get user list

---
+++ src/services/api.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const API_BASE_URL = "https://jsonplaceholder.typicode.com";
+
+const instance = axios.create({
+  baseURL: API_BASE_URL,
+  timeout: 5000,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+export const getUsers = () => {
+  return instance.get("/users");
+};
