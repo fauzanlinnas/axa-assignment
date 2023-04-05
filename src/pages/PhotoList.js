5a86f68b7cb4e5455e3aeb83097eae9817921d6e
Fauzan <fauzan.linnas@ifg-life.id>
2023-04-05 13:54:27 +0700

    add photo list and photo detail

---
+++ src/pages/PhotoList.js
@@ -0,0 +1,66 @@
+import React, { useState, useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { getAlbumDetail } from "../services/api";
+import Modal from "../component/Modal";
+
+const PhotoList = () => {
+  const navigate = useNavigate();
+  const { albumId } = useParams();
+
+  const [photos, setPhotos] = useState([]);
+  const [isSeePhoto, setIsSeePhoto] = useState(false);
+  const [activePhoto, setActivePhoto] = useState(null);
+
+  useEffect(() => {
+    const fetchPhotos = async () => {
+      try {
+        const response = await getAlbumDetail(albumId);
+        setPhotos(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchPhotos();
+  }, [albumId]);
+
+  return (
+    <div>
+      <h2 className="mb-4 text-2xl font-semibold">
+        <button onClick={() => navigate(-1)}>{"<"}</button> Photos
+      </h2>
+      <div className="grid grid-cols-3 gap-4">
+        {photos.map((photo) => (
+          <div
+            key={photo.id}
+            onClick={() => {
+              setIsSeePhoto(true);
+              setActivePhoto(photo);
+            }}
+            className="cursor-pointer"
+          >
+            <img
+              src={photo.thumbnailUrl}
+              alt={photo.title}
+              className="max-w-full h-auto"
+            />
+            <p className="mt-2">{photo.title}</p>
+          </div>
+        ))}
+      </div>
+      <Modal
+        isOpen={isSeePhoto}
+        title={activePhoto?.title}
+        onClose={() => setIsSeePhoto(false)}
+      >
+        <img
+          src={activePhoto.thumbnailUrl}
+          alt={activePhoto.title}
+          className="max-w-full h-auto m-auto"
+        />
+      </Modal>
+    </div>
+  );
+};
+
+export default PhotoList;
