d516b84a95e600714f3676f40fe9efce86d425ad
Fauzan <fauzan.linnas@ifg-life.id>
2023-04-05 13:55:43 +0700

    add route album and post detail

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Container from "./component/Container";
 import Home from "./pages/Home";
 import UserPostsList from "./pages/UserPostsList";
 import AlbumList from "./pages/AlbumList";
+import PhotoList from "./pages/PhotoList";
+import PostDetail from "./pages/PostDetail";
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/users/:id" element={<UserPostsList />} />
         <Route path="/users/:id/album-list" element={<AlbumList />} />
+        <Route path="/users/:id/album-list/:albumId" element={<PhotoList />} />
+        <Route path="/post/:id" element={<PostDetail />} />
       </Routes>
     </Container>
   );
